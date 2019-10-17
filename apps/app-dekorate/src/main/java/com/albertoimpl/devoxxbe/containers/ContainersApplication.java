df77cadb0238b684843b0ea41e00714323aedc2c
Alberto Rios <arios@pivotal.io>
2019-10-17 12:08:44 +0200

    Adding sections on Dekorate and probes

---
+++ apps/app-dekorate/src/main/java/com/albertoimpl/devoxxbe/containers/ContainersApplication.java
@@ -0,0 +1,31 @@
+package com.albertoimpl.devoxxbe.containers;
+
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.annotation.Probe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@KubernetesApplication(
+//		livenessProbe = @Probe(httpActionPath = "/actuator/info"),
+//		readinessProbe = @Probe(httpActionPath = "/actuator/health")
+)
+public class ContainersApplication {
+
+	@RestController
+	static class HelloController {
+
+		@RequestMapping("/")
+		public String home() {
+			return "Hello, All";
+		}
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainersApplication.class, args);
+	}
+
+}
