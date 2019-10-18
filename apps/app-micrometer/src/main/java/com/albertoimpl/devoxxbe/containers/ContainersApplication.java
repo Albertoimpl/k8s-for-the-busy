a344bb541d9b20058588f8c7e788780c2c6fe684
Alberto Rios <arios@pivotal.io>
2019-10-18 15:42:02 +0200

    Adding app with prometheus configured

--- apps/app-micrometer/src/main/java/com/albertoimpl/devoxxbe/containers/ContainersApplication.java
+++ apps/app-micrometer/src/main/java/com/albertoimpl/devoxxbe/containers/ContainersApplication.java
@@ -1,7 +1,12 @@
 package com.albertoimpl.devoxxbe.containers;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ContainersApplication {
 
+	@Value("${app.name}")
+	private String applicationName;
+
 	@RequestMapping("/")
 	public String home() {
-		return "Hello, All";
+		return applicationName;
 	}
 
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", "myapp-micrometer-tag");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContainersApplication.class, args);
